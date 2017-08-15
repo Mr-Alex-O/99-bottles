472dcae9e1877fd7ad5d831b6078a087232fc833
Mr-Alex-O <alexolszewski1993@gmail.com>
2017-08-15 14:57:32 -0400

    got location returning

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const request = require('request');
 const app = express();
+const util = require('util');
 var cors = require('cors')
 const API_KEY = '';
+const ROOT_URL = 'http://api.brewerydb.com/v2/';
 
 app.use(cors());
 
 
-app.get('/locations', function(req,res) {
+app.get('/locations', function(req, res) {
   request
-  .get('http://api.brewerydb.com/v2/locations',{
+  .get(`${ROOT_URL}locations`,{
     qs: {
       locality: req.query.locality,
       region: req.query.region,
+      key: API_KEY
+    }
+  })
+  .pipe(res);
+})
+
+app.get('/brewery/:id', function(req, res) {
+  request
+  .get(`${ROOT_URL}brewery/${req.params.id}`, {
+    qs: {
+      withLocations: "Y",
       key: API_KEY
     }
   })
